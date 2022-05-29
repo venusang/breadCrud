b9ced7ac69afb79338733015fa3ada98ffae844f
Venus Ang <venusang24@gmail.com>
2022-05-28 19:42:43 -0700

    Added the server so that the app can run and do things

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+// CONFIGURATION
+require("dotenv").config();
+const PORT = process.env.PORT;
+const app = express();
+
+// MIDDLEWARE
+app.use(express.static("public"));
+app.set("views", __dirname + "/views");
+app.set("view engine", "jsx");
+app.engine("jsx", require("express-react-views").createEngine());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("<h1>BreadCrud</h1>");
+});
+
+// Breads
+const breadsController = require("./controllers/breads_controller.js");
+app.use("/breads", breadsController);
+
+// 404 Page
+app.get("*", (req, res) => {
+  res.send("404");
+});
+
+app.listen(PORT, () => {
+  console.log("nomming at port", PORT);
+});
